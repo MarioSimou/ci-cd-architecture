a4ad4a4758fae239e255b573b2e25e635682c7a7
MarioSimou <marios.simouu@gmail.com>
2022-12-08 11:22:21 +0000

    feat: cache pnpm store path

--- src/a/index.spec.js
+++ src/a/index.spec.js
@@ -7,11 +7,7 @@ test('Calculator', async t => {
     assert.equal(addition(1, 2), 3)
   })
 
-  await t.test('Simple substraction', () => {
+  await t.test('Substraction', () => {
     assert.equal(substraction(3, 1), 2)
-  })
-
-  await t.test('Complex substraction', () => {
-    assert.equal(substraction(192, 92), 100)
   })
 })
